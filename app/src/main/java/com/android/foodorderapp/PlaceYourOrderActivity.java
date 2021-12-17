7a359bed8fd7f5e58ac64db2ab02daf7f06ef7d0
rezadanang <rezadanang23@gmail.com>
2021-12-17 19:59:50 +0700

    Delivery App
    Delivery App

---
+++ app/src/main/java/com/android/foodorderapp/PlaceYourOrderActivity.java
@@ -0,0 +1,254 @@
+package com.android.foodorderapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.foodorderapp.adapters.PlaceYourOrderAdapter;
+import com.android.foodorderapp.model.Menu;
+import com.android.foodorderapp.model.RestaurantModel;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class PlaceYourOrderActivity extends AppCompatActivity {
+
+    private EditText inputName, inputAddress, inputCity, inputState, inputZip, inputCardNumber, inputCardExpiry, inputCardPin;
+    private RecyclerView cartItemsRecyclerView;
+    private TextView tvSubtotalAmount, tvDeliveryChargeAmount, tvDeliveryCharge, tvTotalAmount, buttonPlaceYourOrder, tvCity, tvState, tvPin, tvAdd, tvCountry;
+    private SwitchCompat switchDelivery;
+    private Button btnAdd;
+    private boolean isDeliveryOn;
+    private PlaceYourOrderAdapter placeYourOrderAdapter;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_your_order);
+
+        RestaurantModel restaurantModel = getIntent().getParcelableExtra("RestaurantModel");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(restaurantModel.getName());
+        actionBar.setSubtitle(restaurantModel.getAddress());
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        inputName = findViewById(R.id.inputName);
+        inputAddress = findViewById(R.id.inputAddress);
+        inputCity = findViewById(R.id.inputCity);
+        inputState = findViewById(R.id.inputState);
+        inputZip = findViewById(R.id.inputZip);
+        inputCardNumber = findViewById(R.id.inputCardNumber);
+        inputCardExpiry = findViewById(R.id.inputCardExpiry);
+        inputCardPin = findViewById(R.id.inputCardPin);
+        tvSubtotalAmount = findViewById(R.id.tvSubtotalAmount);
+        tvDeliveryChargeAmount = findViewById(R.id.tvDeliveryChargeAmount);
+        tvDeliveryCharge = findViewById(R.id.tvDeliveryCharge);
+        tvTotalAmount = findViewById(R.id.tvTotalAmount);
+        tvCity = findViewById(R.id.tvCity);
+        tvState = findViewById(R.id.tvState);
+        tvPin = findViewById(R.id.tvPin);
+        tvAdd = findViewById(R.id.tvAdd);
+        tvCountry = findViewById(R.id.tvCountry);
+        buttonPlaceYourOrder = findViewById(R.id.buttonPlaceYourOrder);
+        btnAdd = findViewById(R.id.btnAdd);
+        switchDelivery = findViewById(R.id.switchDelivery);
+        cartItemsRecyclerView = findViewById(R.id.cartItemsRecyclerView);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(PlaceYourOrderActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(PlaceYourOrderActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
+        buttonPlaceYourOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPlaceOrderButtonClick(restaurantModel);
+            }
+        });
+
+        switchDelivery.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    inputAddress.setVisibility(View.VISIBLE);
+                    inputCity.setVisibility(View.VISIBLE);
+                    inputState.setVisibility(View.VISIBLE);
+                    inputZip.setVisibility(View.VISIBLE);
+                    tvDeliveryChargeAmount.setVisibility(View.VISIBLE);
+                    tvDeliveryCharge.setVisibility(View.VISIBLE);
+                    tvCity.setVisibility(View.VISIBLE);
+                    tvState.setVisibility(View.VISIBLE);
+                    tvPin.setVisibility(View.VISIBLE);
+                    tvAdd.setVisibility(View.VISIBLE);
+                    tvCountry.setVisibility(View.VISIBLE);
+                    btnAdd.setVisibility(View.VISIBLE);
+                    isDeliveryOn = true;
+                    calculateTotalAmount(restaurantModel);
+                } else {
+                    inputAddress.setVisibility(View.GONE);
+                    inputCity.setVisibility(View.GONE);
+                    inputState.setVisibility(View.GONE);
+                    inputZip.setVisibility(View.GONE);
+                    tvDeliveryChargeAmount.setVisibility(View.GONE);
+                    tvDeliveryCharge.setVisibility(View.GONE);
+                    tvCity.setVisibility(View.GONE);
+                    tvState.setVisibility(View.GONE);
+                    tvPin.setVisibility(View.GONE);
+                    tvAdd.setVisibility(View.GONE);
+                    tvCountry.setVisibility(View.GONE);
+                    btnAdd.setVisibility(View.GONE);
+                    isDeliveryOn = false;
+                    calculateTotalAmount(restaurantModel);
+                }
+            }
+        });
+        initRecyclerView(restaurantModel);
+        calculateTotalAmount(restaurantModel);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+
+                    try {
+                        Geocoder geocoder = new Geocoder(PlaceYourOrderActivity.this, Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1
+                        );
+                        tvAdd.setText("Address: "+addresses.get(0).getAddressLine(0));
+                        tvState.setText("State: "+addresses.get(0).getAdminArea());
+                        tvCity.setText("City: "+addresses.get(0).getSubAdminArea());
+                        tvPin.setText("Postal Code: "+addresses.get(0).getPostalCode());
+                        tvCountry.setText("Country: "+addresses.get(0).getCountryName());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
+    private void calculateTotalAmount(RestaurantModel restaurantModel) {
+        int subTotalAmount = 0;
+
+        for(Menu m : restaurantModel.getMenus()) {
+            subTotalAmount += m.getPrice() * m.getTotalInCart();
+        }
+
+        tvSubtotalAmount.setText("Rp"+String.format("%d", subTotalAmount));
+        if(isDeliveryOn) {
+            tvDeliveryChargeAmount.setText("Rp"+String.format("%d", restaurantModel.getDelivery_charge()));
+            subTotalAmount += restaurantModel.getDelivery_charge();
+        }
+        tvTotalAmount.setText("Rp"+String.format("%d", subTotalAmount));
+    }
+
+    private void onPlaceOrderButtonClick(RestaurantModel restaurantModel) {
+        if(TextUtils.isEmpty(inputName.getText().toString())) {
+            inputName.setError("Please enter name ");
+            return;
+        } else if(isDeliveryOn && TextUtils.isEmpty(inputAddress.getText().toString())) {
+            inputAddress.setError("Please enter address ");
+            return;
+        }else if(isDeliveryOn && TextUtils.isEmpty(inputCity.getText().toString())) {
+            inputCity.setError("Please enter city ");
+            return;
+        }else if(isDeliveryOn && TextUtils.isEmpty(inputState.getText().toString())) {
+            inputState.setError("Please enter zip ");
+            return;
+        }else if( TextUtils.isEmpty(inputCardNumber.getText().toString())) {
+            inputCardNumber.setError("Please enter card number ");
+            return;
+        }else if( TextUtils.isEmpty(inputCardExpiry.getText().toString())) {
+            inputCardExpiry.setError("Please enter card expiry ");
+            return;
+        }else if( TextUtils.isEmpty(inputCardPin.getText().toString())) {
+            inputCardPin.setError("Please enter card pin/cvv ");
+            return;
+        }
+        //start success activity..
+        Intent i = new Intent(PlaceYourOrderActivity.this, OrderSucceessActivity.class);
+        i.putExtra("RestaurantModel", restaurantModel);
+        startActivityForResult(i, 1000);
+    }
+
+    private void initRecyclerView(RestaurantModel restaurantModel) {
+        cartItemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        placeYourOrderAdapter = new PlaceYourOrderAdapter(restaurantModel.getMenus());
+        cartItemsRecyclerView.setAdapter(placeYourOrderAdapter);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home :
+                finish();
+            default:
+                //do nothing
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+
+}
